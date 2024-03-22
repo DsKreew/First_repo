9cf7756043b3a3f9e1d5f2d2fce9c76af5a6be9f
DsKreew <dimakrevsun98@gmail.com>
2024-03-22 14:39:01 +0200

    third commit

--- tests.py
+++ tests.py
@@ -23,6 +23,29 @@
 # d = ((x1-x2)**2 + (y1-y2)**2)**0.5
 # print (d)
 
-a = float(input("Введіть сторону квадрата:"))
-P = 4*a
-print(f"Периметр квадрата дорівнює {P}")
+# a = float(input("Введіть сторону квадрата:"))
+# P = 4*a
+# print(f"Периметр квадрата дорівнює {P}")
+
+# Цінова політика продуктів
+price_per_croissant = 1.04
+price_per_glass = 0.34
+price_per_coffee_pack = 4.42
+
+# Кількість кожного продукту
+num_croissants = int(input("Введіть кількість круасанів: "))
+num_glases = int(input("Введіть кількість стаканів: "))
+num_coffee_packs = int(input("Введіть кількість упаковок кави: "))
+
+# Обчислення загальної вартості
+total_cost = num_croissants * price_per_croissant +\
+             num_glases * price_per_glass + \
+             num_coffee_packs * price_per_coffee_pack
+
+# Визначаємо кількість повних доларів і центів
+total_dollars = int(total_cost)
+total_cents = int(total_cost * 100)
+
+# вивід результату
+print(f"Загальна вартість у повних доларах: {total_dollars} доларів")
+print(f"Загальна вартість у центах: {total_cents} центів")
