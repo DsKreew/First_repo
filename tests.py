997efcceaac92f894e6536eb8298255b22c9bee2
DsKreew <dimakrevsun98@gmail.com>
2024-03-30 18:16:44 +0200

    Commit_six

--- tests.py
+++ tests.py
@@ -231,9 +231,136 @@
 # else:
 #     print("Hi Anonym!")
 
-a = [1, 2, 3]
-b = a
-c = [1, 2, 3]
+# a = [1, 2, 3]
+# b = a
+# c = [1, 2, 3]
 
-print(a is b)  # True
-print(a is c)  # False
+# print(a is b)  # True
+# print(a is c)  # False
+
+
+# name = "Dmytro"
+# age = 17
+# has_driver_licence = True
+
+# if name and age >= 18 and has_driver_licence:
+#     print(f"User {name} can rent a car")
+# else:
+#     print(f"User {name} can't rent a car")
+
+# num = int(input("Введіть число: "))
+
+# length = len(str(num))
+
+# if length == 2 and num % 2 == 0:
+#     print(f"{num} - Парне двозначне число")
+# else:
+#     print("Ні")
+
+# num = int(input())
+
+# if num % 3 == 0 and num % 5 == 0:
+#     print('Fizzbuzz')
+# elif num % 3 == 0:
+#     print('Fizz')
+# elif num % 5 == 0:
+#     print('Buzz')
+# else:
+#     print(num)
+
+# x = int(input("X: "))
+# y = int(input("Y: "))
+
+# if x == 0:
+#     print("X can`t be equal to zero")
+#     x = int(input("X: "))
+
+# result = y / x
+# print(result)
+
+
+# x = int(input("X: "))
+# y = int(input("Y: "))
+# if x >= 0:
+#     if y >= 0:  # x > 0, y > 0
+#         print("Перша чверть")
+#     else:  # x > 0, y < 0
+#         print("Четверта чверть")
+# else:
+#     if y >= 0:  # x < 0, y > 0
+#         print("Друга чверть")
+#     else:  # x < 0, y < 0
+#         print("Третя чверть")
+
+# is_nice = False
+# if is_nice:
+#     state = "nice"
+# else:
+#     state = "not nice"
+# print(state)
+
+# is_nice = True
+# state = "nice" if is_nice else "not nice"
+# print(state)
+
+
+# some_data = None
+# msg = some_data or "Не було повернено даних"
+# print(msg)
+
+# fruit = "banana"
+
+# match fruit:
+#     case "apple":
+#         print("This is an apple.")
+#     case "banana":
+#         print("This is a banana.")
+#     case "orange":
+#         print("This is an orange.")
+#     case _:
+#         print("Unknown fruit.")
+
+# point = (0, 0)
+
+# match point:
+#     case (0, 0):
+#         print("Точка в центрі координат")  
+#     case (0, y):
+#         print(f"Точка лежить на осі Y: y={y}")  
+#     case (x, 0):
+#         print(f"Точка лежить на осі X: x={x}") 
+#     case (x, y):
+#         print(f"Точка має координати:  x={x}, y={y}") 
+#     case _:
+#         print("Це не точка")
+
+# pets = ["dog", "fish", "cat"]
+
+# match pets:
+#     case ["dog", _, "cat"]:
+#         # Випадок, коли є і собака, і кіт
+#         print("There's a dog and a cat.")
+#     case ["dog", _, _]:
+#         # Випадок, коли є тільки собака
+#         print("There's a dog.")
+#     case _:
+#         # Випадок для інших комбінацій
+#         print("No dogs.")
+
+# fruit = 'apple'
+# for char in fruit:
+#     print(char)
+
+
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# for char in alphabet:
+#     print(char, end=" ")
+
+# some_iterable = ["a", "b", "c"]
+
+# for i in some_iterable:
+#     print(i)
+
+odd_numbers = [1, 3, 5, 7, 9]
+for i in odd_numbers:
+    print(i ** 2)
